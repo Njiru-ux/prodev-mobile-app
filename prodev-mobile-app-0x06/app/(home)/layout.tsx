2d97463fcd87162820ef083c8fafd531c627e485
Njiru-ux <ericbishop99@gmail.com>
2025-11-22 19:02:30 +0300

    Updated layout.tsx

--- prodev-mobile-app-0x06/app/(home)/layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+
 const HomeRootLayout = () => {
   return (
     <Tabs screenOptions={{
@@ -20,16 +21,17 @@ const HomeRootLayout = () => {
         headerShown: true,
         tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />
       }} />
-        <Tabs.Screen name="inbox" options={{
+      <Tabs.Screen name="inbox" options={{
         title: 'Inbox',
         headerShown: true,
         tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
       }} />
-        <Tabs.Screen name="profile" options={{
+      <Tabs.Screen name="profile" options={{
         title: 'Profile',
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
   )
 }
+
 export default HomeRootLayout;
