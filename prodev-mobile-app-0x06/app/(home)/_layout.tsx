a4e1fb75c469340f603a48bcbf6b7bab88c85e3e
Njiru-ux <ericbishop99@gmail.com>
2025-11-22 14:59:54 +0300

    Updating layout.tsx

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -28,10 +28,11 @@ const HomeRootLayout = () => {
       }} />
       <Tabs.Screen name="profile" options={{
         title: 'Profile',
+        headerShown: false,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
-  );
-};
+  )
+}
 
 export default HomeRootLayout;
